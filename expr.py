0ef7ab41c576d4247e112460458e14f23543479b
Xitong Gao <gxtfmx@gmail.com>
2013-01-23 15:36:44 +0000

    Add skeleton expr.py

---
+++ expr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim: set fileencoding=UTF-8 :
+
+
+__author__ = 'Xitong Gao'
+__email__ = 'xtg08@ic.ac.uk'
+
+
+class Expr(object):
+    """Expr - data structure for arithmetic expressions"""
+    pass
