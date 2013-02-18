fe7b1a7cbd80777c23c284b7ca12ecb5c48a5f65
Xitong Gao <gxtfmx@gmail.com>
2013-02-18 16:50:54 +0000

    Allow equivalence checking for Expr

--- expr/parser.py
+++ expr/parser.py
@@ -52,12 +52,27 @@ class Expr(object):
             self.arg1 = _try_to_number(arg1)
             self.arg2 = _try_to_number(arg2)
 
+    def tuple(self):
+        def to_tuple(a):
+            if isinstance(a, Expr):
+                return a.tuple()
+            return a
+        return (self.op, to_tuple(self.arg1), to_tuple(self.arg2))
+
     def __str__(self):
         return '(%s %s %s)' % (str(self.arg1), self.op, str(self.arg2))
 
     def __repr__(self):
         return "Expr(op='%s', arg1=%s, arg2=%s)" % \
                 (self.op, repr(self.arg1), repr(self.arg2))
+
+    def __eq__(self, other):
+        if not isinstance(other, Expr):
+            return False
+        return self.tuple() == other.tuple()
+
+    def __hash__(self):
+        return hash(self.tuple())
 
 
 if __name__ == '__main__':
