f2dc9b260e6ca1fcf46b9f23fad5478ab7ff28f8
Xitong Gao <gxtfmx@gmail.com>
2013-03-05 16:38:11 +0000

    Fix dict argument not hashable

--- ce/expr/common.py
+++ ce/expr/common.py
@@ -11,12 +11,22 @@ MULTIPLY_OP = '*'
 OPERATORS = [ADD_OP, MULTIPLY_OP]
 
 
+def to_immutable(*m):
+    def r(d):
+        if isinstance(d, dict):
+            return tuple((e, to_immutable(v)) for e, v in d.iteritems())
+        if isinstance(d, (list, tuple)):
+            return tuple(to_immutable(e) for e in d)
+        return d
+    return tuple(r(e) for e in m)
+
+
 _cache_map = dict()
 
 
 def cached(f):
     def decorated(*args, **kwargs):
-        key = (f, tuple(args), tuple(kwargs.items()))
+        key = to_immutable(f, args, kwargs.items())
         if key in _cache_map:
             return _cache_map[key]
         v = f(*args, **kwargs)
