fbd4a702ae61359b4489c83766f8d360da8bb85e
Xitong Gao <gxtfmx@gmail.com>
2013-03-12 16:08:16 +0000

    Preliminary Area object

--- ce/semantics/area.py
+++ ce/semantics/area.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python
 # vim: set fileencoding=UTF-8 :
+
+
+from . import Lattice
+
+
+class Area(Lattice):
+
+    def __init__(self, e):
+        self.e = e
+        self.l, self.s = e.as_labels()
+        super(Area, self).__init__()
+
+    def join(self, other):
+        pass
+
+    def meet(self, other):
+        pass
+
+    def __add__(self, other):
+        return Area(self.e + other.e)
+
+    def __sub__(self, other):
+        return Area(self.e - other.e)
+
+    def __mul__(self, other):
+        return Area(self.e * other.e)
