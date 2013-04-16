b1bd9ed21cec6a99845b4bab44bb64eb0e11db18
Xitong Gao <gxtfmx@gmail.com>
2013-04-16 15:34:28 +0100

    Update Martel's traces

--- ce/transformer/utils.py
+++ ce/transformer/utils.py
@@ -30,19 +30,23 @@ def parsings(tree):
 
 class MartelExpr(Expr):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def traces(self, depth):
         def subtraces(a):
             try:
-                return a.traces(depth)
-            except AttributeError:
+                return MartelExpr(a).traces(depth)
+            except (ValueError, TypeError):
                 return {a}
         stl = [subtraces(a) for a in self.args]
-        cll = [closure(Expr(self.op, args), depth=depth)
-               for args in itertools.product(*stl)]
-        return set.intersection(*cll)
+        sts = set(Expr(self.op, args) for args in itertools.product(*stl))
+        logger.debug('Generating %s~=%d traces for tree: %s' %
+                     ('*'.join([str(len(s)) for s in stl]),
+                      len(sts), str(self)))
+        cll = closure(sts, depth=depth)
+        return cll
+
+    def __repr__(self):
+        return "MartelExpr(op='%s', a1=%s, a2=%s)" % \
+            (self.op, repr(self.a1), repr(self.a2))
 
 
 def martel(tree, depth=3):
