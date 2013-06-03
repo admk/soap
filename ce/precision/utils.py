1f9f109a029ca281bedbd71e642068b98007e276
Xitong Gao <gxtfmx@gmail.com>
2013-06-03 18:19:46 +0100

    Fix precision utils

--- ce/precision/utils.py
+++ ce/precision/utils.py
@@ -7,7 +7,10 @@ from ce.precision.common import PRECISIONS
 
 
 def precision_context(prec):
-    prec += 1
+    try:
+        prec += 1
+    except TypeError:
+        raise TypeError('Precision must be an integer, found %s.' % prec)
     return gmpy2.local_context(gmpy2.ieee(128), precision=prec)
 
 
@@ -21,38 +24,33 @@ def set_precision_recursive(expr, prec):
 
 
 def precision_permutations(expr, prec_list=PRECISIONS):
-    from ce.expr import Expr, BARRIER_OP, is_expr
-    if not is_expr(expr):
-        s = []
-        for e in expr:
-            s += precision_permutations(e, prec_list)
-        return s
+    from ce.expr import Expr, BARRIER_OP
     try:
-        p1, p2 = [precision_permutations(a, prec_list) for a in expr.args]
+        with logger.local_context(l=logger.levels.off):
+            p1, p2 = [precision_permutations(a, prec_list) for a in expr.args]
         if expr.op == BARRIER_OP:
             prec_list = [None]
         elif not expr.prec is None:
             prec_list = [expr.prec]
-        s = []
+        s = set()
         n = len(p1) * len(p2) * len(prec_list)
         i = 0
         for a1, a2 in itertools.product(p1, p2):
             for p in prec_list:
                 i += 1
-                logger.persistent('Permutation', '%d/%d' % (i, n),
-                                  l=logger.levels.debug)
-                s.append(Expr(expr.op, a1, a2, prec=p))
+                if i % 100 == 0:
+                    logger.persistent('Permutation', '%d/%d' % (i, n),
+                                      l=logger.levels.debug)
+                s.add(Expr(expr.op, a1, a2, prec=p))
         logger.unpersistent('Permutation')
         return s
     except AttributeError:
-        return [expr]
+        return {expr}
 
 
-def precision_variations(expr_or_set, prec_list=PRECISIONS):
-    from ce.expr import Expr, is_expr
-    if is_expr(expr_or_set):
-        return [Expr(expr_or_set, prec=p) for p in prec_list]
-    s = []
-    for e in expr_or_set:
-        s += precision_variations(e, prec_list)
+def precision_variations(expr, prec_list=PRECISIONS):
+    from ce.expr import Expr
+    s = set()
+    for p in prec_list:
+        s |= precision_permutations(expr, prec_list=[p])
     return s
