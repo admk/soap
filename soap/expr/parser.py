99be4b07c1baf1f5d8c2cabd56290fde7f982ff9
Xitong Gao <gxtfmx@gmail.com>
2013-08-21 17:49:41 +0100

    Refactor _parse_r into ast_to_expr for reuse

--- soap/expr/parser.py
+++ soap/expr/parser.py
@@ -40,6 +40,33 @@ def raise_parser_error(desc, text, token):
     raise exc
 
 
+def ast_to_expr(t, cls, s):
+    with ignored(AttributeError):
+        return t.n
+    with ignored(AttributeError):
+        return t.id
+    with ignored(AttributeError):
+        return t.s
+    with ignored(AttributeError):
+        return tuple(ast_to_expr(v, cls, s) for v in t.elts)
+    try:
+        op = OPERATOR_MAP[t.op.__class__]
+        if op == UNARY_SUBTRACT_OP:
+            return -ast_to_expr(t.operand, cls, s)
+        a1 = ast_to_expr(t.left, cls, s)
+        a2 = ast_to_expr(t.right, cls, s)
+        if op == DIVIDE_OP:
+            try:
+                return gmpy2.mpq(a1, a2)
+            except TypeError:
+                pass
+        return cls(op, a1, a2)
+    except KeyError:
+        raise_parser_error('Unrecognised operator %s' % str(t.op), s, t)
+    except AttributeError:
+        raise_parser_error('Unknown token %s' % str(t), s, t)
+
+
 def parse(s, cls):
     """Parses a string into an instance of class `cls`.
 
@@ -49,35 +76,9 @@ def parse(s, cls):
     :type cls: types.ClassType
     """
     s = s.replace('\n', '').strip()
-
-    def _parse_r(t):
-        with ignored(AttributeError):
-            return t.n
-        with ignored(AttributeError):
-            return t.id
-        with ignored(AttributeError):
-            return t.s
-        with ignored(AttributeError):
-            return tuple(_parse_r(v) for v in t.elts)
-        try:
-            op = OPERATOR_MAP[t.op.__class__]
-            if op == UNARY_SUBTRACT_OP:
-                return -_parse_r(t.operand)
-            a1 = _parse_r(t.left)
-            a2 = _parse_r(t.right)
-            if op == DIVIDE_OP:
-                try:
-                    return gmpy2.mpq(a1, a2)
-                except TypeError:
-                    pass
-            return cls(op, a1, a2)
-        except KeyError:
-            raise_parser_error('Unrecognised operator %s' % str(t.op), s, t)
-        except AttributeError:
-            raise_parser_error('Unknown token %s' % str(t), s, t)
     try:
         body = ast.parse(s, mode='eval').body
-        return _parse_r(body)
+        return ast_to_expr(body, cls, s)
     except (TypeError, AttributeError):
         raise TypeError('Parse argument must be a string')
     except SyntaxError as e:
