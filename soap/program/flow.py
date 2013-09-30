a6e9375410172439696171a461df869ec422903e
Xitong Gao <gxtfmx@gmail.com>
2013-09-30 13:19:15 +0100

    Update comments on __alt_flow
    Just realised this is not true. Because eventually we are dealing with an
    analysis that gives *reachable states per label* in the program.

--- soap/program/flow.py
+++ soap/program/flow.py
@@ -82,17 +82,7 @@ class WhileFlow(Flow):
         return state.conditional(self.conditional_expr, False)
 
     def __alt_flow(self, state):
-        """An alternative approach to :member:`flow`.
-
-        The problem is whether we want the *reachable* values of variables,
-        or the values of variables after execution of statements. The former
-        provides the correct range of values in which the expressions should be
-        optimised, while the latter provides a more precise analysis necessary
-        for the loopy parts of the program to provide a more precise correct
-        range of values in loop kernels. Currently it is not clear to me
-        which flow function is more suitable for both cases, I keep this
-        implementation here for later considerations.
-        """
+        """An alternative approach to :member:`flow`."""
         branch = lambda state, cond: \
             state.conditional(self.conditional_expr, cond)
         prev_state = false_state = state.__class__(bottom=True)
