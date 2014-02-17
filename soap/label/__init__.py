ab68a987dd927b74a6ee28ee298328afdd886f5b
Xitong Gao <gxtfmx@gmail.com>
2014-02-17 01:18:23 +0000

    Add all_iterations for iterating iterations

--- soap/label/__init__.py
+++ soap/label/__init__.py
@@ -1,5 +1,5 @@
 from soap.label.base import fresh_int, Label
-from soap.label.iteration import Iteration
+from soap.label.iteration import Iteration, all_iterations
 from soap.label.annotation import Annotation
 from soap.label.identifier import Identifier
 
