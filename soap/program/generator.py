e54981d6712509a12ff7995b91221c807ae87957
Xitong Gao <gxtfmx@gmail.com>
2014-05-03 05:19:48 +0100

    Preliminary generator framework

--- soap/program/generator.py
+++ soap/program/generator.py
@@ -1,11 +1,12 @@
 import copy
 
-from soap.expression import is_variable, is_expression
+from soap.expression import is_variable, is_expression, StateGetterExpr
 from soap.label import Label
+from soap.program.flow import AssignFlow, CompositionalFlow
 from soap.semantics import is_numeral
 
 
-def _dependency_dictionary(env, variables):
+def _dependency_dictionary(env, vars):
     """
     Extract dependencies from env.
 
@@ -16,7 +17,7 @@ def _dependency_dictionary(env, variables):
         deps == {x: {j, k}, y: {l, k}, h: set(), j: set(), k: {j, h}, l: set()}
     """
     deps = {}
-    for var in variables:
+    for var in vars:
         expr = env.get(var)
 
         # find dependent variables for the corresponding expression
@@ -24,6 +25,10 @@ def _dependency_dictionary(env, variables):
             # can't find expression for var or var is an input variable, so
             # there are no dependencies for it
             expr_deps = set()
+        elif isinstance(expr, StateGetterExpr):
+            raise NotImplementedError(
+                'TODO special treatment for this, because of the labelling '
+                'behaviour.')
         elif is_expression(expr):
             # dependent variables in the expression
             expr_deps = expr.vars()
@@ -87,9 +92,30 @@ def _dependency_order(deps):
         yield from _dependency_order(deps)
 
 
-def dependency_order(env, variables):
-    yield from _dependency_order(_dependency_dictionary(env, variables))
+def dependency_order(env, vars):
+    yield from _dependency_order(_dependency_dictionary(env, vars))
 
 
-def generate(env, variables):
-    ...
+class _Generator(object):
+    def generate(self, env, vars):
+        deps = dependency_order(env, vars)
+        flows = []
+        for vars in deps:
+            for var in vars:
+                expr = env[var]
+                method = getattr(
+                    self, 'generate_' + expr.__class__.__name__,
+                    self.generic_generate)
+                flow = method(var, expr)
+                if isinstance(flow, CompositionalFlow):
+                    flows += flow.flows
+                else:
+                    flows.append(flow)
+        return CompositionalFlow(flows)
+
+    def generic_generate(self, var, expr):
+        return AssignFlow(var, expr)
+
+
+def generate(env, vars):
+    return _Generator().generate(env, vars)
