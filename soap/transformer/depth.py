7022539696ea607eb9958b3d706f38faf979b5a7
Xitong Gao <gxtfmx@gmail.com>
2014-07-21 10:14:43 +0200

    Add bound to labels in crop

--- soap/transformer/depth.py
+++ soap/transformer/depth.py
@@ -1,25 +1,29 @@
 from soap.common import base_dispatcher
-from soap.expression import expression_factory
-from soap.semantics import LabelContext
+from soap.expression import BoolExpr, expression_factory
+from soap.semantics import arith_eval, LabelContext
 
 
-class Cropper(base_dispatcher('crop', 'crop')):
-    def generic_crop(self, expr, depth, context):
+class Cropper(base_dispatcher('crop')):
+    def generic_crop(self, expr, state, depth, context):
         raise TypeError('Do not know how to crop expression {!r}'.format(expr))
 
-    def _crop_atom(self, expr, depth, context):
+    def _crop_atom(self, expr, state, depth, context):
         return expr, {}
 
     crop_numeral = _crop_atom
     crop_Variable = _crop_atom
 
-    def _crop_expression(self, expr, depth, context):
+    def _crop_expression(self, expr, state, depth, context):
         if depth <= 0:
-            label = context.Label(expr)
+            if isinstance(expr, BoolExpr):
+                bound = None
+            else:
+                bound = arith_eval(expr, state)
+            label = context.Label(expr, bound)
             return label, {label: expr}
 
         cropped_args = tuple(
-            self(arg, depth - 1, context) for arg in expr.args)
+            self(arg, state, depth - 1, context) for arg in expr.args)
         args_label, args_env = zip(*cropped_args)
 
         expr = expression_factory(expr.op, *args_label)
@@ -30,22 +34,21 @@ class Cropper(base_dispatcher('crop', 'crop')):
 
         return expr, env
 
-    def crop_FixExpr(self, expr, depth, context):
-        return expr, {}
+    crop_FixExpr = _crop_atom
 
     crop_BinaryArithExpr = _crop_expression
     crop_BinaryBoolExpr = _crop_expression
     crop_SelectExpr = _crop_expression
 
-    def _execute(self, expr, depth, context=None):
+    def _execute(self, expr, state, depth, context=None):
         context = context or LabelContext(expr)
-        return super()._execute(expr, depth, context)
+        return super()._execute(expr, state, depth, context)
 
 
-class Stitcher(base_dispatcher('stitch', 'stitch')):
+class Stitcher(base_dispatcher('stitch')):
     def generic_stitch(self, expr, env):
-        raise TypeError('Do not know how to stitch expression {!r}'
-                        .format(expr))
+        raise TypeError(
+            'Do not know how to stitch expression {!r}'.format(expr))
 
     def _stitch_atom(self, expr, env):
         return expr
