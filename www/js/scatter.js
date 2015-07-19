50d1c4c3fc843b21c9f7d024781bd317082b0666
John Wickerson <johnwickerson@cantab.net>
2015-07-19 17:49:12 +0100

    Fabulous new web interface! Graphs no longer overlap at certain window sizes. Data point for "original" program is slightly larger. Examples spread out horizontally. Changed "Luts" to "LUT count", and "Latency" to "Latency (cycles)" if that's correct -- do feel free to revert that bit if incorrect. I also thought it would be nice to have a textual description of each test that appears when one clicks on its name.

--- www/js/scatter.js
+++ www/js/scatter.js
@@ -1,14 +1,35 @@
-var margin = {top: 20, right: 20, bottom: 100, left: 40},
-    width = 300 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+
+var description_of = {};
+description_of["sum"] = "Curabitur egestas lorem nunc, eu volutpat augue interdum eget. Mauris placerat risus non dui pretium, ut tristique elit interdum. Nam in quam nunc. Nullam id auctor eros, ut aliquet erat. Integer in convallis ex. Mauris vehicula tortor eget cursus sagittis. Integer eget pharetra risus. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean posuere hendrerit est, eget bibendum nisl malesuada in. Nulla facilisi. Quisque dictum tellus non turpis tristique feugiat. Sed gravida nunc at augue molestie, at convallis est suscipit. Aenean porttitor nulla non elit dapibus ornare nec eget mauris. Ut viverra, eros nec lobortis posuere, velit lectus tincidunt mauris, non volutpat odio neque nec est. Integer vitae dui bibendum, ultrices felis ac, congue massa.";
+description_of["inner_product"] = "Cras rutrum mollis faucibus. Morbi at nunc dapibus, rhoncus sapien eu, tincidunt lacus. Ut sodales porttitor felis ut hendrerit. Aenean luctus, mi non accumsan efficitur, nibh erat dapibus orci, eget imperdiet eros nunc nec libero. Integer suscipit augue tempor, molestie quam non, ornare tellus. Aliquam erat volutpat. Proin convallis vehicula quam, vitae dignissim augue sollicitudin et. Donec vitae arcu neque.";
+description_of["2mm"] = "Etiam at ipsum mauris. Maecenas tincidunt commodo turpis, et mollis arcu. Cras viverra eu dui quis eleifend. Quisque dignissim augue nec rutrum feugiat. Nunc placerat luctus imperdiet. In hac habitasse platea dictumst. Duis sit amet nisl nisi. Nunc luctus metus est, id efficitur dolor dictum eget. Phasellus elementum sollicitudin efficitur. Praesent a nisl viverra, elementum tellus id, lobortis felis. Maecenas elit velit, interdum quis libero at, fermentum rutrum lacus. Sed sollicitudin lacinia quam, vel pharetra risus mattis tincidunt. Fusce sed augue aliquet, venenatis libero ut, accumsan diam. Praesent ac nisi lobortis, venenatis tellus tempor, porttitor ante. Phasellus quis ornare sem.";
+description_of["3mm"] = "In hac habitasse platea dictumst. Donec dapibus accumsan interdum. Cras sit amet ultrices nisl. Phasellus sagittis libero vestibulum, semper dui nec, viverra lorem. Maecenas vitae tortor erat. In gravida nisi ut tellus iaculis maximus. Nunc eget tempus tellus, et finibus nisi. Vestibulum quis libero ultrices, mattis libero tincidunt, vestibulum augue. Sed porta sed urna vitae tempus. Nunc fringilla nunc sed neque ultrices, et efficitur sem bibendum. Curabitur quis egestas leo. Sed posuere augue eget justo sagittis, non scelerisque mi pharetra. In vel metus tellus. Integer fermentum tempor magna, et ornare est egestas a. Nullam at urna augue.";
+description_of["atax"] = "Sed at dui porta, luctus ante et, maximus neque. Cras tincidunt molestie leo at suscipit. Integer auctor leo in sapien tincidunt, ac laoreet metus semper. Phasellus pretium pharetra eleifend. In porta massa risus, faucibus placerat ex tincidunt eget. Donec volutpat quam dolor, ac congue nunc aliquet sed. Fusce ex mauris, viverra non lacus in, tincidunt scelerisque lorem. Praesent commodo rutrum risus, id lobortis velit. Nam interdum efficitur leo sit amet faucibus. Nullam eu leo enim. Pellentesque non tempus massa. Vivamus vehicula turpis at consectetur viverra. Cras ex nulla, dictum vitae rhoncus vel, eleifend eu augue. Vestibulum feugiat felis ac sagittis placerat. Vivamus ultricies eleifend ligula, ac ultricies sem luctus eu. Fusce eu ullamcorper justo, in congue nunc.";
+description_of["bicg"] = "Pellentesque quis sagittis risus. Fusce augue tortor, commodo at felis non, aliquam vestibulum purus. Phasellus sollicitudin mauris nulla, vitae mattis metus porttitor id. Fusce et viverra dui, nec congue eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam ac velit eleifend, viverra odio id, vestibulum urna. Duis auctor mauris vel dolor dictum semper. Etiam eu libero nec lacus consectetur lobortis.";
+description_of["gemm"] = "Fusce vestibulum in ante ut pellentesque. Pellentesque in consequat metus, in accumsan mi. Sed vel aliquet ligula, eu euismod massa. Donec ut quam eget risus eleifend tempus. Aenean ut ultrices ligula, ac ornare nulla. Fusce est dui, aliquam at ante pretium, ullamcorper lobortis justo. Donec mollis ligula non felis faucibus, sit amet volutpat erat placerat. Cras tellus diam, varius eget tincidunt eget, tincidunt id augue. Nulla molestie tristique ante, sit amet varius mauris luctus ut. Vivamus vitae eros elementum, placerat sem non, blandit ligula.";
+description_of["gemver"] = "Proin commodo nunc id orci scelerisque, et dapibus est maximus. Morbi convallis ipsum in neque gravida elementum. Nullam in hendrerit mauris, in vulputate leo. Aliquam erat volutpat. Praesent vehicula dolor et justo iaculis, vel bibendum dolor ullamcorper. Fusce scelerisque lacus vulputate, pulvinar purus nec, porttitor nisi. Sed blandit cursus diam, sit amet vehicula arcu rutrum a.";
+description_of["mvt"] = "Praesent vitae felis iaculis, pretium urna sit amet, interdum est. Pellentesque et lorem consequat, pretium massa eget, tempus diam. Donec lacinia laoreet augue, a egestas elit viverra id. Etiam vitae lacus porttitor, aliquet erat vel, efficitur velit. Morbi non justo ac sapien efficitur lobortis. In ac dolor rutrum, auctor velit eget, cursus nisi. Nunc neque ipsum, tincidunt sed dictum eget, dictum et nisi. Fusce tempus sem eget risus consequat fringilla. Suspendisse dignissim quam dui, vel volutpat ex accumsan nec. Mauris aliquam sem vel tincidunt ultricies. Nullam auctor dictum metus vel pulvinar.";
+description_of["syr2k"] = "Praesent interdum nisl dolor, nec lacinia nisl facilisis at. Phasellus facilisis, felis at aliquet vestibulum, dolor velit lobortis lacus, nec interdum sapien turpis eget tortor. Vestibulum et blandit neque, et egestas tellus. Aliquam sit amet enim convallis, lacinia enim id, laoreet dolor. Proin urna ligula, porttitor ut lectus vitae, condimentum pellentesque diam. Phasellus vehicula metus non ante aliquet, a venenatis metus ultricies. Sed placerat leo nec sem fringilla, non tristique magna suscipit. Phasellus ut eros id metus finibus aliquam. Cras eget cursus dolor.";
+description_of["seidel"] = "Etiam vel ultrices metus. Ut malesuada velit quis urna tempor interdum. Etiam a lacus ligula. Vivamus sodales metus ut mauris tempus consequat. Morbi congue posuere vehicula. Integer ornare, quam quis efficitur bibendum, ligula orci tristique erat, in mollis enim massa vitae sapien. Nunc suscipit interdum quam vitae faucibus. Phasellus eu lectus molestie, ultricies nibh sit amet, aliquam ligula. Integer tristique enim vitae felis bibendum, nec sollicitudin nunc rutrum. Aliquam dapibus ante mauris, in posuere lectus auctor vitae. Etiam vitae nulla ultricies, ultrices risus eget, suscipit diam. Maecenas dictum eros quis congue euismod.";
 
 var usage_msg =
-    "[Hover over a data-point in one of the graphs to see the corresponding program.]"
+    "[Hover over a data-point in one of the graphs below to see the corresponding program.]"
 
+var min_graph_size = 450
 
+var normal_dot_size = 3.5
+var original_dot_size = 7
+var hover_dot_size = 7
 
-function draw_graph(data, xValue, xdata_label, yValue, ydata_label, graphzone,tooltip) {
+function draw_graph(data, xValue, xdata_label, yValue, ydata_label, graphzone, tooltip, orig_program_textarea, opti_program_textarea) {
 
+  var grossWidth = Math.min(min_graph_size, parseInt(graphzone.style('width'), 10));
+  var grossHeight = grossWidth;
+  
+  var margin = {top: 20, right: 20, bottom: 100, left: 40},
+      width = grossWidth - margin.left - margin.right,
+      height = grossHeight - margin.top - margin.bottom;
+  
   /*
    * value - returns the value to encode for a given data object.
    * scale - maps value to a visual display encoding, such as a pixel position.
@@ -29,12 +50,15 @@ function draw_graph(data, xValue, xdata_label, yValue, ydata_label, graphzone,to
   var axisformatter = d3.format(".3s");
 
   // add the graph canvas to the body of the webpage
-  var svg = d3.select("#"+graphzone)
+  var svg = graphzone
       .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", grossWidth)
+      .attr("height", grossHeight)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
 
 
   // don't want dots overlapping axis, so add in buffer to data domain
@@ -76,14 +100,14 @@ function draw_graph(data, xValue, xdata_label, yValue, ydata_label, graphzone,to
     .attr("dy", ".71em")
     .style("text-anchor", "end")
     .text(ydata_label);
-
+  
   // draw dots
   svg.selectAll(".dot")
     .data(data)
     .enter()
     .append("circle")
     .attr("class", "dot")
-    .attr("r", 3.5)
+    .attr("r", normal_dot_size)
     .attr("cx", xMap)
     .attr("cy", yMap)
     .attr("eid", function(d) { return d.eid; })
@@ -92,21 +116,20 @@ function draw_graph(data, xValue, xdata_label, yValue, ydata_label, graphzone,to
     .style("opacity",0.5)
     .on("mouseover", function(d) {
       d3.selectAll(".dot")
-    .style("fill", "black")
-    .attr("r", 3.5);
+	.style("fill", "black")
+	.attr("r", normal_dot_size);
       d3.selectAll(".dot[eid='0']")
-    .style("fill", "white")
-    .attr("r", 5);;
+	.style("fill", "white")
+	.attr("r", original_dot_size);;
       d3.selectAll(".dot[eid='"+d.eid+"']")
-    .style("fill", "red")
-    .attr("r", 5);
-      d3.select('#opti_program')
-    .text(d.expression);
+	.style("fill", "red")
+	.attr("r", hover_dot_size);
+      opti_program_textarea.text(d.expression);
       tooltip.transition()
         .duration(200)
         .style("opacity", .9);
       tooltip.html(xdata_label + ": " + axisformatter(xValue(d)) + "<br />" +
-           ydata_label + ": " + axisformatter(yValue(d)))
+		   ydata_label + ": " + axisformatter(yValue(d)))
         .style("left", (d3.event.pageX + 5) + "px")
         .style("top", (d3.event.pageY - 28) + "px");
     })
@@ -115,20 +138,28 @@ function draw_graph(data, xValue, xdata_label, yValue, ydata_label, graphzone,to
         .duration(500)
         .style("opacity", 0);
     });
-
+  
 
   d3.selectAll(".dot[eid='0']")
     .style("fill","white")
-    .attr("r", 5);
+    .attr("r", original_dot_size);
 
-  d3.select('#orig_program')
-    .text(data[0].expression);
+  orig_program_textarea.text(data[0].expression);
 
 }
 
-function draw_graphs(csv_file) {
+var csv_file = "sum";
 
-  csv_file = csv_file.value;
+var run_once = false;
+var example_description;
+var graphzone1;
+var graphzone2;
+var graphzone3;
+var tooltip;
+var orig_program_textarea;
+var opti_program_textarea;
+
+function redraw () {
 
   // load data
   d3.csv("csv/" + csv_file + ".csv", function(_, data) {
@@ -149,21 +180,77 @@ function draw_graphs(csv_file) {
 
     // Remove any previous graphs
     d3.selectAll("svg").remove();
-    d3.select('#opti_program').text(usage_msg);
+    
+    if (!run_once) {
+      run_once = true;
+      
+      // add the tooltip area to the webpage
+      tooltip = d3.select("body")
+	.append("div")
+	.attr("class", "tooltip")
+	.style("opacity", 0);
 
-    // add the tooltip area to the webpage
-    var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+      example_description = d3.select("#main_container")
+	.append("div")
+	.attr("class", "col-md-12")
+	.append("p");
 
+      var orig_program = d3.select("#main_container")
+	  .append("div")
+	  .attr("class", "col-md-6");
+
+      var opti_program = d3.select("#main_container")
+	  .append("div")
+	  .attr("class", "col-md-6");
+
+      orig_program.append("h3").text("Original program");
+      orig_program_textarea = orig_program.append("textarea")
+	.style("width","100%")
+	.attr("rows","20");
+
+      opti_program.append("h3").text("Optimized program");
+      opti_program_textarea = opti_program.append("textarea")
+	.style("width","100%")
+	.attr("rows","20")
+	.text(usage_msg);
+      
+      graphzone1 = d3.select("#main_container")
+	.append("div")
+	.attr("class", "col-md-4")
+      
+      graphzone2 = d3.select("#main_container")
+	.append("div")
+	.attr("class", "col-md-4");
+      
+      graphzone3 = d3.select("#main_container")
+	.append("div")
+	.attr("class", "col-md-4");
+      
+      graphzone1.append("h3").text("Latency vs. error");
+      graphzone2.append("h3").text("LUT count vs. error");
+      graphzone3.append("h3").text("LUT count vs. latency");
+    }
+
+    example_description.text(description_of[csv_file]);
+      
     var get_error = function(d) {return d.error},
     get_latency = function(d) {return d.latency},
     get_lut = function(d) {return d.lut};
 
-    draw_graph(data, get_error, "error", get_latency, "latency", "graphzone1",tooltip);
-    draw_graph(data, get_error, "error", get_lut, "lut", "graphzone2", tooltip);
-    draw_graph(data, get_latency, "latency", get_lut, "lut", "graphzone3",tooltip);
+    draw_graph(data, get_error, "error", get_latency, "latency (cycles)", graphzone1,
+	       tooltip, orig_program_textarea, opti_program_textarea);
+    draw_graph(data, get_error, "error", get_lut, "LUT count", graphzone2,
+	       tooltip, orig_program_textarea, opti_program_textarea);
+    draw_graph(data, get_latency, "latency (cycles)", get_lut, "LUT count", graphzone3,
+	       tooltip, orig_program_textarea, opti_program_textarea);
 
   });
 
 }
+
+function draw_graphs(new_csv_file) {
+  csv_file = new_csv_file;
+  redraw ();
+}
+
+//window.onresize = redraw; 
